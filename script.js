96a45335c920377abe831bc1211387b5bf01469f
Rishabh kumar <83425253+Rishabhk0207@users.noreply.github.com>
2024-05-09 22:15:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const makeChange = (c) => {
   // your name here
+	 const values = {
+        "q": 25,
+        "d": 10,
+        "n": 5,
+        "p": 1
+    };
+
+    let result = {};
+
+    for (let coin in values) {
+		
+        result[coin] = Math.floor(c / values[coin]);
+        c %= values[coin];
+    }
+
+    return result; 
+	
 };
 
 // Do not the change the code below
